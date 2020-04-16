b395bc54d51daa5877991debf22e80197df37cfc
Nitin Ranganath <nitinranganath@gmail.com>
2020-04-16 22:06:58 +0530

    Set up API routes

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 // Connect to database
 connectDB();
 
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.get("/", (req, res) => {
     res.send("Hello World !");
 });
