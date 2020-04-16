f2a266fd24bc5c872ef4a30fa18d3bc59bf223f3
Nitin Ranganath <nitinranganath@gmail.com>
2020-04-16 20:45:00 +0530

    Initial setup and MongoDB connection

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("MongoURI");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB connected ...");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Hello World !");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
